0f81fa0729325d891d479cd2473ab46687979b79
Henrique Becker Brum <45078469+HenriqueBBrum@users.noreply.github.com>
2019-04-13 19:41:07 -0300

    Almost done

--- t2/t2.hs
+++ t2/t2.hs
@@ -2,6 +2,7 @@
 import Data.Char
 import System.IO
 import System.Exit
+import Data.Fixed
 
 type Point     = (Float,Float)
 type Rect      = (Point,Float,Float)
@@ -13,27 +14,7 @@ type Circle    = (Point,Float)
 -------------------------------------------------------------------------------
 
 rgbPalette :: Int -> [(Int,Int,Int)]
-rgbPalette n = take n $ cycle [(255,0,0),(0,255,0),(0,0,255),(255,255,0)]
-
-rgbPalette2 :: Int -> [(Int,Int,Int)]
-rgbPalette2 n  = init (greenToblue (x+rest)) ++ init (blueToRed x) ++ init (redToGreen x)
-    where x = n `div` 3
-          rest = n `mod` 3
-
-greenToblue :: Int -> [(Int,Int,Int)]
-greenToblue 0 = [(0,0,0)]
-greenToblue n = [(0,g,b) | g<-[255,255-(255 `div` n)..0], b<-[0,(255 `div` n)..255], b == abs(g-255)]
-
-blueToRed :: Int -> [(Int,Int,Int)]
-blueToRed 0 = [(0,0,0)]
-blueToRed n = [(r,0,b) | r<-[0,(255 `div` n)..255], b<-[255,255-(255`div` n)..0], r == abs(b-255)]
-
-redToGreen :: Int -> [(Int,Int,Int)]
-redToGreen 0 = [(0,0,0)]
-redToGreen n = [(r,g,0) | r<-[255,255-(255`div` n)..0], g<-[0,(255 `div` n)..255],g == abs(r-255)]
-
-rgbPalette3 :: Int -> [(Int,Int,Int)]
-rgbPalette3 n  = take (x+rest) (strongToWeak (x+rest) 'r') ++ take x (strongToWeak x 'g') ++ take x (strongToWeak x 'b')
+rgbPalette n  = take (x+rest) (strongToWeak (x+rest) 'r') ++ take x (strongToWeak x 'g') ++ take x (strongToWeak x 'b')
     where x = n `div` 3
           rest = n `mod` 3
 
@@ -46,22 +27,58 @@ strongToWeak n c
   |otherwise = [(x,x-(x`div`2),x-(x`div`2)) | x <-[0,(255 `div` n)..255]]
 
 
---generate a list of n tuple where every z tuple the green part goes back to the
---same green as z-x
-completeGreenPalette :: Int -> Int -> [(Int,Int,Int)]
-completeGreenPalette n x
-  |n `mod` x == 0 = greenNPalette (n `div` x) x 80
-  |otherwise =  greenNPalette (n `div` x) x 80 ++ greenPalette (n `mod` x) lastInit
-  where lastInit  = fromIntegral (n `div` x)*20+80
+fadeColor :: (Int, Int, Int) -> [(Int, Int, Int)]
+fadeColor color  = [color]
 
-greenNPalette :: Int ->Int -> Int -> [(Int,Int,Int)]
-greenNPalette n x y
-    | n == 0 = []
-    |otherwise = greenPalette x y ++ greenNPalette (n-1) x (y+10)
+trd (_,_,a) = a
+first (a,_,_) = a
+second (_,a,_) = a
 
-greenPalette :: Int -> Int -> [(Int,Int,Int)]
-greenPalette n init = [(0,init+i*10,0) | i <- [0..(n-1)] ]
+takeN :: Int -> [Int]
+takeN n
+  |n > 360 =  take n $ cycle [0..360]
+  |otherwise = take n $ cycle [0,(360 `div` n)..360]
 
+rainbowPalette :: Int  -> [(Int, Int, Int)]
+rainbowPalette n = [rainbowPalette2 x |x <- takeN n]
+
+rainbowPalette2 :: Int -> (Int, Int, Int)
+rainbowPalette2 angle
+  |angle < 60 = (255, hsvLights !! angle, 0)
+  |angle < 120 = (hsvLights !! (120-angle), 255, 0)
+  |angle < 180 = ( 0, 255, hsvLights !! (angle-120))
+  |angle < 240 = (0, hsvLights !! (240-angle), 255)
+  |angle < 300 = (hsvLights !! (angle-240), 0, 255)
+  |otherwise = (255, 0, hsvLights !! (360-angle))
+  where hsvLights = part1 ++ part2 ++ part3 ++ part4
+        part1 = [0, 4, 8, 13, 17, 21, 25, 30, 34, 38, 42, 47, 51, 55, 59, 64, 68, 72, 76]
+        part2 = [81, 85, 89, 93, 98, 102, 106, 110, 115, 119, 123, 127, 132, 136, 140, 144]
+        part3 = [149, 153, 157, 161, 166, 170, 174, 178, 183, 187, 191, 195, 200, 204, 208]
+        part4 = [212, 217, 221, 225, 229, 234, 238, 242, 246, 251, 255]
+
+
+createRgbCopy :: Float-> Float -> Float -> (Float, Float, Float)
+createRgbCopy h c x
+  | h>= 0 && h<60 = (c,x,0)
+  | h>= 60 && h<120 = (x,c,0)
+  | h>= 120 && h<180 = (0,c,x)
+  | h>= 180 && h<240 = (0,x,c)
+  | h>= 240 && h<300 = (x,0,c)
+  | h>= 300 && h<360 = (c,0,x)
+
+returnMod :: Float -> Float -> Float
+returnMod x y = x `mod'` y
+
+hsvToRgb :: (Float, Float, Float) -> (Int, Int, Int)
+hsvToRgb hsv
+  |first hsv>360 || first hsv<0 = (0,0,0)
+  |second hsv < 0.0 || second hsv >1.0 = (255,255,255)
+  |trd  hsv < 0.0 || trd hsv >1.0 = (125,0,125)
+  |otherwise =  (round((first rgbCopy + m)*255), round((second rgbCopy + m)*255), round((trd rgbCopy + m)*255))
+  where rgbCopy = createRgbCopy (first hsv) c x
+        m = trd hsv - c
+        x = c*(1-abs( returnMod (first hsv/60) 2 - 1))
+        c = second hsv * trd hsv
 
 
 
@@ -147,7 +164,7 @@ genCurvilinearLine n i y
         initP = (x,y)
 
 createCircle2 ::Int -> Float -> Float -> Point -> [Circle]
-createCircle2  n r angle p  = [((100.0 + fst p, snd p + cos(angle)*4*(fromIntegral n)/r), r)]
+createCircle2  n r angle p  = [((100.0 + fst p,100+ snd p + cos(angle)*4*(fromIntegral n)/r), r)]
 
 -------------------------------------------------------------------------------
 -- Gera��o de circulo fromado por varios retangulos
@@ -246,37 +263,52 @@ mapDigitsToInt str = joinInt (map digitToInt str)
 joinInt :: [Int] -> Int
 joinInt l = read $ map intToDigit l
 
-genCase1::Int -> Int -> String
-genCase1 nrects nrectperline = svgElements svgRect rects (map svgStyle palette)
+
+adjustColor:: String -> String -> String -> String -> (Int, Int, Int)
+adjustColor x y z hsv
+  |hsv == "y" =  hsvToRgb ((read x ::Float), (read y ::Float), (read z ::Float))
+  |otherwise = ((read x ::Int), (read y ::Int), (read z ::Int))
+
+rainbowOrNot ::Int -> Int -> String -> (Int, Int, Int) -> [(Int, Int, Int)]
+rainbowOrNot cs n rw color
+  | rw == "y" = rainbowPalette (n)
+  | cs ==  1 || cs == 2 = fadeColor color
+  | cs == 3 = take (n) $ cycle [(255,0,0),(0,255,0),(0,0,255)]
+  | cs == 4 = rgbPalette n
+  | cs == 5 = take (n) $ cycle [(255,0,0),(0,255,0),(0,0,255),(255,255,0)]
+  | cs == 6 = take n $ cycle [color]
+
+
+genCase1::Int -> Int -> (Int,Int,Int) ->String -> String
+genCase1 nrects nrectperline color rw = svgElements svgRect rects (map svgStyle palette)
   where rects = genAllRects nrects nrectperline
-        palette = completeGreenPalette nrects nrectperline
+        palette = rainbowOrNot 1 nrects rw color
 
-genCase2::Int -> Float -> Point -> String
-genCase2 ncircles r p = svgElements svgCircle circles (map svgStyle palette)
+genCase2::Int -> Float -> Point -> (Int,Int,Int) -> String -> String
+genCase2 ncircles r p color rw = svgElements svgCircle circles (map svgStyle palette)
   where circles = genCirclesInCircleFormation ncircles (p, r)
-        palette = rgbPalette2 ncircles
+        palette = rainbowOrNot 2 ncircles rw color
 
-genCase3 ::Int -> Int -> Float -> String
-genCase3 ncirclescomb combPerLine r = svgElements svgCircle circles (map svgStyle palette)
+genCase3 ::Int -> Int  -> Float -> String -> String
+genCase3 ncirclescomb combPerLine r rw = svgElements svgCircle circles (map svgStyle palette)
   where circles = genCirclesInTiangularFormation ncirclescomb combPerLine r
-        palette = take (3*ncirclescomb) $ cycle [(255,0,0),(0,255,0),(0,0,255)]
+        palette  = rainbowOrNot 3 (3*ncirclescomb) rw (0,0,0)
 
-
-genCase4::Int -> String
-genCase4 ncircles = svgElements svgCircle circles (map svgStyle palette)
+genCase4::Int -> String -> String
+genCase4 ncircles rw = svgElements svgCircle circles (map svgStyle palette)
   where circles = genCirclesInCurvilinearFormation ncircles
-        palette = rgbPalette3 ncircles
+        palette = rainbowOrNot 4 ncircles rw (0,0,0)
 
 
 --Does something cool
-genCase5::Int -> Int-> Float -> Point ->String
-genCase5 nC nR r p = svgElements svgRect rects (map svgStyle palette)
+genCase5::Int -> Int-> Float -> Point -> String ->String
+genCase5 nC nR r p rw = svgElements svgRect rects (map svgStyle palette)
   where rects = genRectanglesInCircleFormation nC nR (p,r)
-        palette = rgbPalette (nC*nR)
+        palette = rainbowOrNot 5 (nC*nR) rw (0,0,0)
 
-genCase6::Int -> Int -> (Int,Int,Int) -> String
-genCase6 mf mi  color = svgElements svgRect rects (map svgStyle palette)
-  where palette = [color | n <-[0..length rects]]
+genCase6::Int -> Int -> (Int,Int,Int)-> String -> String
+genCase6 mf mi color rw = svgElements svgRect rects (map svgStyle palette)
+  where palette = rainbowOrNot 6 (length rects) rw color
         rects = genRectanglesInMdSet (300.0,300.0) (2.0, 1.6) mf mi
 
 prompt :: String -> IO String
@@ -285,62 +317,69 @@ prompt text = do
   hFlush stdout
   getLine
 
---Abrir o caso 3 e 5 tanto no google quanto no explorer para ver as diferenças
 
 main :: IO()
 main = do
   putStrLn "Try case 3, 5 and 6 both on google and windows\n"
-  putStrLn "Choose a case :"
   putStrLn "case 1 : A green table\ncase 2 : A circle formed of circles"
   putStrLn "case 3 : A grid of Borromean rings\ncase 4 : Three curves formed by circles"
   putStrLn "case 5 : Something interesting that happened by chance\ncase 6 : Mandelbrot set"
 
-  caseVar <- getChar
-  aux<- prompt ""
+  caseVar<- prompt "Choose a case: "
 
-  if(caseVar<'1' || caseVar >'6') then do
+  if((read caseVar :: Int) < 1 || (read caseVar :: Int) > 6) then do
       putStr "You chose a unvalid number"
       exitFailure
   else
       putStrLn "You chose a valid number "
 
   let (w,h) = (1500.0,1000.0)
-  printf "Width = %.2f Height =%.2f \n" w h
-  printf "Case %c\n" caseVar
+  printf "Screen -> Width = %.2f Height =%.2f \n\n" w h
+
+  rainbow <- prompt "To use rainbow palette type 'y', for normal palette type any other key: "
+
+  putStrLn "This next options will only work if you chose normal palette for case 1,2 or 6"
+  putStrLn "If your choices don't fall on that category just press they key 0 four times"
+  putStrLn "Choose a color"
+  hsv <- prompt "To use HSV type 'y', for RGB type any other key:  "
+  putStrLn "If you chose hsv, the fst value is between 0 and 360 , the snd and trd are 0.0 and 1.0"
+  x<- prompt "Type the firt value of your color model: "
+  y<- prompt "Type the second value of your color model: "
+  z<- prompt "Type the thrid value of your color model: "
+  let color = (adjustColor x y z hsv)
+
+  printf "\nCase %s\n" caseVar
 
   case(caseVar) of
-      x | x == '1' -> do
+      x | x == "1" -> do
           n1<- prompt "Type how many rects: "
           n2<- prompt "Type how many rects per line: "
-          writeFile "caseX.svg" $ svgBegin w h ++ genCase1 (read n1 :: Int) (read n2 :: Int) ++ svgEnd
-      x | x == '2' -> do
+          writeFile "caseX.svg" $ svgBegin w h ++ genCase1 (read n1 :: Int) (read n2 :: Int) color rainbow ++ svgEnd
+      x | x == "2" -> do
           px<- prompt "Type X point where this case will be drawn: "
           py<- prompt "Type Y point where this case will be drawn : "
           let p = ((read px :: Float),(read py :: Float))
           n1<- prompt "Type how many circles: "
           n2<- prompt "Type the radius of the bigger circle: "
-          writeFile "caseX.svg" $ svgBegin w h ++ genCase2 (read n1 :: Int) (read n2 :: Float) p ++ svgEnd
-      x | x == '3' -> do
+          writeFile "caseX.svg" $ svgBegin w h ++ genCase2 (read n1 :: Int) (read n2 :: Float) p color rainbow ++ svgEnd
+      x | x == "3" -> do
           n1<- prompt "Type how many combinations: "
           n2<- prompt "Type the amount of combinations per line "
           n3<- prompt "Type the radius of every circle: "
-          writeFile "caseX.svg" $ svgBegin w h ++ genCase3  (read n1 :: Int) (read n2 :: Int) (read n3 :: Float) ++ svgEnd
-      x | x == '4' -> do
+          writeFile "caseX.svg" $ svgBegin w h ++ genCase3  (read n1 :: Int) (read n2 :: Int) (read n3 :: Float) rainbow ++ svgEnd
+      x | x == "4" -> do
           n1<- prompt "Type how many circles in total: "
-          writeFile "caseX.svg" $ svgBegin w h ++ genCase4 (read n1 :: Int) ++ svgEnd
-      x | x == '5' -> do
+          writeFile "caseX.svg" $ svgBegin w h ++ genCase4 (read n1 :: Int) rainbow ++ svgEnd
+      x | x == "5" -> do
           px<- prompt "Type X point where this case will be drawn: "
           py<- prompt "Type Y point where this case will be drawn : "
           let p = ((read px :: Float),(read py :: Float))
           n1<- prompt "Type how many pairs of circles: "
           n2<- prompt "Type the amount of rects per pair of circle "
           n3<- prompt "Type the proportion of the radius of the smallest circle: "
-          writeFile "caseX.svg" $ svgBegin w h ++ genCase5  (read n1 :: Int) (read n2 :: Int) (read n3 :: Float) p ++ svgEnd
-      x | x == '6' -> do
-          putStr "This case some time to build so watch out for the maximum iteration\n"
+          writeFile "caseX.svg" $ svgBegin w h ++ genCase5  (read n1 :: Int) (read n2 :: Int) (read n3 :: Float) p rainbow ++ svgEnd
+      x | x == "6" -> do
+          putStr "This case takes some time to build so watch out for the maximum iteration\n"
           n1<- prompt "Type the magnification factor: "
           n2<- prompt "Type the maximum interation: "
-          r<- prompt "Type the value of the r in RGB: "
-          g<- prompt "Type the value of the g in RGB: "
-          b<- prompt "Type the value of the b in RGB: "
-          writeFile "caseX.svg" $ svgBegin w h ++ genCase6 (read n1 :: Int)  (read n1 :: Int) ((read n1 :: Int),(read n1 :: Int),(read n1 :: Int)) ++ svgEnd
+          writeFile "caseX.svg" $ svgBegin w h ++ genCase6 (read n1 :: Int)  (read n2 :: Int) color rainbow ++ svgEnd
