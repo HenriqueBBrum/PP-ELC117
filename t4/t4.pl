74c31fd9dfe47879a75e94f32e71ca74ed5e8ddc
Henrique Becker Brum <45078469+HenriqueBBrum@users.noreply.github.com>
2019-05-03 22:33:32 -0300

    Add files via upload

---
+++ t4/t4.pl
@@ -0,0 +1,84 @@
+assasino(X):- motivo(X), acesso(X).
+
+acesso(X):- temChave(X),roubouArma(X),estavaApart(X),!.
+
+temChave(bia).
+
+temChave(X):-localizacao(X,portoAlegre,terca);localizacao(X,santaMaria,quarta).
+
+roubouArma(X):-roubouMartelo(X);roubouBaseballBat(X).
+
+roubouBaseballBat(X):- localizacao(X,portoAlegre,quinta);localizacao(X,santaMaria,quarta), relacao(X,bernardo),!.
+
+roubouMartelo(X):- localizacao(X,apartamento,quinta);localizacao(X,apartamento,quarta).
+
+estavaApart(X):-localizacao(X,apartamento,quinta);localizacao(X,apartamento,sexta).
+
+motivo(X):- insanidade(X);ciumes(X);dinheiro(X).
+
+insanidade(X):- insano(X).
+ciumes(X):- ciumento(X).
+dinheiro(X):- pobre(X).
+
+
+ciumento(X):- relacao(anita,Y), relacao(X,Y).
+ciumento(X):- relacao(anita,Y), relacao(Y,X).
+
+relacao(anita,bernardo).
+relacao(bernardo,caren).
+relacao(anita,pedro).
+relacao(pedro,alice).
+relacao(alice,henrique).
+relacao(henrique,maria).
+relacao(maria,adriano).
+relacao(adriano,caren).
+
+vitima(anita).
+
+insano(adriano).
+insano(maria).
+
+pobre(bernardo).
+pobre(bia).
+pobre(pedro).
+pobre(maria).
+
+%segunda não importa
+localizacao(pedro,santaMaria,terca).
+localizacao(pedro,portoAlegre,quarta).
+localizacao(pedro,santaMaria,quinta).
+localizacao(pedro,apartamento,sexta).
+
+localizacao(caren,portoAlegre,terca).
+localizacao(caren,portoAlegre,quarta).
+localizacao(caren,santaMaria,quinta).
+localizacao(caren,apartamento,sexta).
+
+localizacao(henrique,portoAlegre,terca).
+localizacao(henrique,apartamento,quarta).
+localizacao(henrique,apartamento,quinta).
+localizacao(henrique,apartamento,sexta).
+
+localizacao(bia,portoAlegre,terca).
+localizacao(bia,portoAlegre,quarta).
+localizacao(bia,santaMaria,quinta).
+localizacao(bia,apartamento,sexta).
+
+localizacao(adriano,santaMaria,quarta).
+localizacao(adriano,apartamento,quinta).
+localizacao(adriano,apartamento,sexta).
+
+localizacao(alice,portoAlegre,terca).
+localizacao(alice,portoAlegre,quarta).
+localizacao(alice,apartamento,quinta).
+localizacao(alice,apartamento,sexta).
+
+localizacao(bernardo,santaMaria,terca).
+localizacao(bernardo,portoAlegre,quarta).
+localizacao(bernardo,santaMaria,quinta).
+localizacao(bernardo,apartamento,sexta).
+
+localizacao(maria,santaMaria,terca).
+localizacao(maria,santaMaria,quarta).
+localizacao(maria,santaMaria,quinta).
+localizacao(maria,apartamento,sexta).
