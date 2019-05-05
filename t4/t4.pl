fbe661cb2407ef6bf1f9b28a1b34085fa1c4ce3e
Henrique Becker Brum <45078469+HenriqueBBrum@users.noreply.github.com>
2019-05-05 18:29:46 -0300

    Add files via upload

--- t4/t4.pl
+++ t4/t4.pl
@@ -4,7 +4,7 @@ acesso(X):- temChave(X),roubouArma(X),estavaApart(X),!.
 
 temChave(bia).
 
-temChave(X):-localizacao(X,portoAlegre,terca);localizacao(X,santaMaria,quarta).
+temChave(X):-localizacao(X,portoAlegre,terca);localizacao(X,santaMaria,santaMaria).
 
 roubouArma(X):-roubouMartelo(X);roubouBaseballBat(X).
 
@@ -44,40 +44,49 @@ pobre(pedro).
 pobre(maria).
 
 %segunda não importa
+localizacao(pedro,santaMaria,segunda).
 localizacao(pedro,santaMaria,terca).
 localizacao(pedro,portoAlegre,quarta).
 localizacao(pedro,santaMaria,quinta).
 localizacao(pedro,apartamento,sexta).
 
+localizacao(caren,portoAlegre,segunda).
 localizacao(caren,portoAlegre,terca).
 localizacao(caren,portoAlegre,quarta).
 localizacao(caren,santaMaria,quinta).
 localizacao(caren,apartamento,sexta).
 
+localizacao(henrique,apartamento,segunda).
 localizacao(henrique,portoAlegre,terca).
 localizacao(henrique,apartamento,quarta).
 localizacao(henrique,apartamento,quinta).
 localizacao(henrique,apartamento,sexta).
 
+localizacao(bia,apartamento,segunda).
 localizacao(bia,portoAlegre,terca).
 localizacao(bia,portoAlegre,quarta).
 localizacao(bia,santaMaria,quinta).
 localizacao(bia,apartamento,sexta).
 
+localizacao(adriano,apartamento,segunda).
+localizacao(adriano,apartamento,terca).
 localizacao(adriano,santaMaria,quarta).
 localizacao(adriano,apartamento,quinta).
 localizacao(adriano,apartamento,sexta).
 
+localizacao(alice,apartamento,segunda).
 localizacao(alice,portoAlegre,terca).
 localizacao(alice,portoAlegre,quarta).
 localizacao(alice,apartamento,quinta).
 localizacao(alice,apartamento,sexta).
 
+localizacao(bernardo,santaMaria,segunda).
 localizacao(bernardo,santaMaria,terca).
 localizacao(bernardo,portoAlegre,quarta).
 localizacao(bernardo,santaMaria,quinta).
 localizacao(bernardo,apartamento,sexta).
 
+localizacao(maria,apartamento,segunda).
 localizacao(maria,santaMaria,terca).
 localizacao(maria,santaMaria,quarta).
 localizacao(maria,santaMaria,quinta).
